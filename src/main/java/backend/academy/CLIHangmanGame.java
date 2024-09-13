7181b4d8fddb8fc65c8860a268d5c2924dd050aa
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-09-13 16:33:32 +0300

    refactor(main for CLI game): add ability to retry to enter your setting

--- src/main/java/backend/academy/CLIHangmanGame.java
+++ src/main/java/backend/academy/CLIHangmanGame.java
@@ -15,7 +15,6 @@ import backend.academy.hangman.game.services.impl.io.ScannerCLIGameSetupReaderIm
 import backend.academy.hangman.game.services.impl.text.WordMetaLoader;
 import backend.academy.hangman.game.services.impl.text.WordStorage;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.Set;
 
 public class CLIHangmanGame {
@@ -25,37 +24,54 @@ public class CLIHangmanGame {
     private static final RandomSetterImpl randomSetter = new RandomSetterImpl();
 
     public static void main(String[] args) {
-        WordMetaLoader loader = tryToInitLoader();
+        WordMetaLoader loader;
 
-        assert loader != null;
+        try {
+            loader = new WordMetaLoader();
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+            return;
+        }
+
         String category = getCategory(loader);
         String difficulty = getDifficulty(loader, category);
         int attempts = getAttempts();
 
+        tryToStartGame(category, difficulty, attempts);
+    }
+
+    private static void tryToStartGame(String category, String difficulty, int attempts) {
         try {
             String word = WordStorage.getRandomWord(category, difficulty);
             printer.println("The word has been chosen, let the game begin!\n");
-            printer.println("category is " + category + " of " + difficulty + " complexity ");
+            printer.println("Category is " + category + " of " + difficulty + " complexity ");
             var game = new CLIHangmanGameFactoryImpl().createHangmanGame(attempts, word);
 
             game.move();
         } catch (IOException e) {
-            System.out.println(":(");
+            System.out.println("Something went wrong");
         }
     }
 
     private static String getCategory(WordMetaLoader loader) {
         String categories = loader.getCategoriesAsString();
         Set<String> setOfCategories = loader.getCategoriesAsSet();
-        printer.println("Choose a category: " + categories);
-        String category = reader.readSettingForWordAsString();
+        String category = setOfCategories.iterator().next();
+        boolean isValid = false;
 
-        try {
-            validator.validateCategory(category, setOfCategories);
-        } catch (IncorrectCategoryException e) {
-            printer.println(e.getMessage());
-        } catch (CategoryIsNotSetException e) {
-            category = randomSetter.getRandomCategory(setOfCategories);
+        while (!isValid) {
+            printer.println("Choose a category: " + categories);
+            category = reader.readSettingForWordAsString();
+
+            try {
+                validator.validateCategory(category, setOfCategories);
+                isValid = true;
+            } catch (IncorrectCategoryException e) {
+                printer.println(e.getMessage());
+            } catch (CategoryIsNotSetException e) {
+                category = randomSetter.getRandomCategory(setOfCategories);
+                isValid = true;
+            }
         }
 
         return category;
@@ -64,50 +80,47 @@ public class CLIHangmanGame {
     private static String getDifficulty(WordMetaLoader loader, String category) {
         String difficulties = loader.getDifficultiesAsString(category);
         Set<String> setOfDifficulties = loader.getDifficultiesAsSet(category);
-        printer.println("Choose difficulty: " + difficulties);
-        String difficulty = reader.readSettingForWordAsString();
+        String difficulty = setOfDifficulties.iterator().next();
+        boolean isValid = false;
 
-        try {
-            validator.validateDifficulty(difficulty, setOfDifficulties);
-        } catch (IncorrectDifficultyException e) {
-            printer.println(e.getMessage());
-        } catch (DifficultyIsNotSetException e) {
-            difficulty = randomSetter.getRandomDifficulty(setOfDifficulties);
+        while (!isValid) {
+            printer.println("Choose difficulty: " + difficulties);
+            difficulty = reader.readSettingForWordAsString();
+
+            try {
+                validator.validateDifficulty(difficulty, setOfDifficulties);
+                isValid = true;
+            } catch (IncorrectDifficultyException e) {
+                printer.println(e.getMessage());
+            } catch (DifficultyIsNotSetException e) {
+                difficulty = randomSetter.getRandomDifficulty(setOfDifficulties);
+                isValid = true;
+            }
         }
 
         return difficulty;
     }
 
     private static int getAttempts() {
-        printer.println("Choose the number of attempts from 1 to 26");
-        String stringAttempts = reader.readSettingForWordAsString();
         int attempts = 6;
+        boolean isValid = false;
 
-        try {
-            validator.validateAttempts(stringAttempts);
-            attempts = Integer.parseInt(stringAttempts);
-        } catch (IncorrectAttemptsException e) {
-            printer.println(e.getMessage());
-        } catch (AttemptsAreNotSetException e) {
-            attempts = randomSetter.getRandomAttempts();
+        while (!isValid) {
+            printer.println("Choose the number of attempts from 1 to 26");
+            String stringAttempts = reader.readSettingForWordAsString();
+
+            try {
+                validator.validateAttempts(stringAttempts);
+                attempts = Integer.parseInt(stringAttempts);
+                isValid = true;
+            } catch (IncorrectAttemptsException e) {
+                printer.println(e.getMessage());
+            } catch (AttemptsAreNotSetException e) {
+                attempts = randomSetter.getRandomAttempts();
+                isValid = true;
+            }
         }
 
         return attempts;
-    }
-
-    private static WordMetaLoader tryToInitLoader() {
-        WordMetaLoader loader;
-
-        try {
-            loader = new WordMetaLoader();
-        } catch (IOException e) {
-            System.out.println(":(");
-            return null;
-        } catch (URISyntaxException e) {
-            System.out.println(":(");
-            return null;
-        }
-
-        return loader;
     }
 }
